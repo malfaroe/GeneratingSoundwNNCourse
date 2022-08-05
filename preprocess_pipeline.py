a1a026caacbd61f15e9473d57981ace2cc6c6348
malfaroe <mauricio.alfaro.espinoza@gmail.com>
2022-08-05 16:38:07 -0400

    Updating pipeline

--- preprocess_pipeline.py
+++ preprocess_pipeline.py
@@ -57,12 +57,30 @@ class Padder:
                                 mode = self.mode)
         return padded_array
 
-    
-    
+
+
+   
 
 class LogSpectrogramExtractor:
-    pass
+    """Extracts the log spectrogram (in dB) from
+    the audiofile signal using the librosa
+    amplitude_to_dB method"""
 
+    def __init__(self, frame_size, hop_length): 
+        self.frame_size = frame_size
+        self.hop_length = hop_length
+
+    #Use the stft for extracting the signal
+
+    def extract(self, signal):
+        stft = librosa.stft(signal, 
+        n_fft= self.frame_size,
+        hop_length= self.hop_length)[0]
+        spectrogram = np.abs(stft)
+        log_spectrogram = librosa.amplitude_to_db(spectrogram)
+        return log_spectrogram
+
+    
 
 class MinMaxNormaliser:
     pass
@@ -75,5 +93,25 @@ class Saver:
 class PreprocessingPipeline:
     pass
 
+
+
+##testing
+FRAME_SIZE = 512
+HOP_LENGTH = 256
+DURATION = 0.74  # in seconds
+SAMPLE_RATE = 22050
+MONO = True
+
+if __name__ == "__main__":
+    arr_m = np.array([1,2,3])
+    song_path = r"C:\Users\malfaro\Desktop\mae_code\GeneratingSoundwNNCourse\spoken_data\7_theo_47.wav"
+    padder = Padder()
+    print("Array with left pad:", padder.left_pad(array = arr_m, num_missing_items = 5))
+    print(padder.left_pad(array = arr_m, num_missing_items = 5).shape)
+    Loader = Loader(SAMPLE_RATE, DURATION, MONO)
+    signal = Loader.load(song_path)
+    print(signal.shape)
+    ls = LogSpectrogramExtractor(FRAME_SIZE, HOP_LENGTH)
+    print(ls.extract(signal).shape)
 
 print("Done.")
