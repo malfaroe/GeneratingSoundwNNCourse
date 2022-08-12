9cbe3d4c28bc8206d5fc20c5becc55f1761ea63a
malfaroe <mauricio.alfaro.espinoza@gmail.com>
2022-08-12 12:11:51 -0400

    Soundgenerator unit complete

--- soundgenerator.py
+++ soundgenerator.py
@@ -26,8 +26,34 @@ class SoundGenerator:
         min_max_values)
         return signal, latent_representations
 
-def convert_spectrograms_to_audio(self, min_max_values):
-    pass
+def convert_spectrograms_to_audio(self, generated_spectrograms,min_max_values):
+    """Converts the generated spectrograms
+    to audio files:
+    1. Reshapes each spec to 2 Dimension arrays [:,:,0] (the incoming input specs will have 3D)
+    [:,:,0] : mantains the 1st and second dimension and drops the third
+    2. Denormalise 
+    3. Convert from log to amplitude
+    4. Apply inverse transform (Griffin-Lim)
+    5. Appends to list of audios"""
+
+    signals = []
+    for spectrogram, min_max_value in zip(generated_spectrograms, min_max_values):
+        #Reshaping
+        log_spectrogram = spectrogram[:,:,0]
+        #Denormalise
+        denorm_log_spec = self.min_max_normaliser.denormalise(norm_array = log_spectrogram,
+                        original_min = min_max_value["min"], 
+                        original_max =  min_max_value["max"])
+        #Convert to amplitude
+        spec = librosa.db_to_amplitude(denorm_log_spec)
+        #Apply inverse transform (Griffin-Lim)
+        signal = librosa.istft(spec, hop_length= self.hop_length)
+        #Append
+        signals.append(signal)
+
+        
+        
+
 
 
         
